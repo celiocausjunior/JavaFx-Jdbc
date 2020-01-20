2afe732f7996fb07f6052de58c41728aa26b8140
Celio Caus Junior <celiocausjunior@gmail.com>
2020-01-19 21:58:22 -0300

    Remove department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -8,23 +8,25 @@ import model.entities.Department;
 
 public class DepartmentService {
 
-	
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
-	
+
 	public List<Department> findAll() {
 
-	return dao.findAll();
+		return dao.findAll();
 
 	}
-	
+
 	public void saveOrUpdate(Department obj) {
 		if (obj.getId() == null) {
 			dao.insert(obj);
-		}
-		else {
+		} else {
 			dao.update(obj);
 		}
-		
+
+	}
+
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
 	}
 
 }
